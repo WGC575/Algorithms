374926b56279b4bf6df1f74e9a38d0c9eaef2cf8
WGC575 <wgc575@gmail.com>
2018-07-20 07:56:17 +0800

    13 completed

--- Leetcode/13_Roman_to_Integer/ri.cpp
+++ Leetcode/13_Roman_to_Integer/ri.cpp
@@ -4,7 +4,7 @@ int main(int argc, char* argv[])
 {
 	string input = "IV";
 	Solution sol = Solution();
-	cout << sol.romanToInt(input);
+	cout << sol.romanToInt(input);	
 
 	system("pause");
 	return 0;
